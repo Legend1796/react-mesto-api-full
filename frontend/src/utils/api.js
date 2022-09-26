0f0510772485d12af34452d95ea6f99536384585
Igor Manukhov <m_igor97@mail.ru>
2022-09-26 23:00:21 +0300

    change baseurl on front

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -100,7 +100,7 @@ export class Api {
 }
 
 const api = new Api({
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://api.legend.students.nomoredomains.sbs',
   headers: {
     'Content-Type': 'application/json'
   }
