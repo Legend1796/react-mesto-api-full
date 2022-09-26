5d058d74dfec20296a9466b62399dd455e5fe0ab
Igor Manukhov <m_igor97@mail.ru>
2022-09-26 23:57:52 +0300

    return on sors old

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-// const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -11,40 +10,44 @@ const { errorHandler } = require('./middlewares/errorHandler');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const allowedCors = [
-  'http://legend.students.nomoredomains.sbs',
-  'https://legend.students.nomoredomains.sbs',
-  'http://api.legend.students.nomoredomains.sbs',
-  'https://api.legend.students.nomoredomains.sbs',
-];
+// const allowedCors = [
+//   'http://legend.students.nomoredomains.sbs',
+//   'https://legend.students.nomoredomains.sbs',
+//   'http://api.legend.students.nomoredomains.sbs',
+//   'https://api.legend.students.nomoredomains.sbs',
+// ];
 
-// app.use(cors({
-//   origin: 'https://legend.students.nomoredomains.sbs',
-//   credentials: true,
-// }));
-// app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
+app.use(cors({
+  origin: [
+    'http://legend.students.nomoredomains.sbs',
+    'https://legend.students.nomoredomains.sbs',
+    'http://api.legend.students.nomoredomains.sbs',
+    'https://api.legend.students.nomoredomains.sbs',
+  ],
+  credentials: true,
+}));
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Credentials', true);
+//   const { origin } = req.headers;
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin);
+//   }
+//   const { method } = req;
+//   const requestHeaders = req.headers['access-control-request-headers'];
 
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-    return;
-  }
-  next();
-});
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     res.end();
+//     return;
+//   }
+//   next();
+// });
 
 app.use(router);
 app.use(errorLogger);
