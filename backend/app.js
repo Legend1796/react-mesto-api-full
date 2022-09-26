e3345b08192329bd5beb659d4d882319bd8e0e4c
Igor Manukhov <m_igor97@mail.ru>
2022-09-26 23:51:44 +0300

    rebuild cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -10,13 +11,39 @@ const { errorHandler } = require('./middlewares/errorHandler');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-app.use(cors({
-  origin: 'https://api.legend.students.nomoredomains.sbs',
-  credentials: true,
-}));
+const allowedCors = [
+  'http://legend.students.nomoredomains.sbs',
+  'https://legend.students.nomoredomains.sbs',
+];
+
+// app.use(cors({
+//   origin: 'https://legend.students.nomoredomains.sbs',
+//   credentials: true,
+// }));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+    return;
+  }
+  next();
+});
+
 app.use(router);
 app.use(errorLogger);
 app.use(errorHandler);
