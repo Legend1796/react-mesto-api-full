1a73a1685164d79d7120b488e8a8c5a1459aed80
Igor Manukhov <m_igor97@mail.ru>
2022-09-27 20:55:45 +0300

    create cors

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -10,46 +10,50 @@ const { errorHandler } = require('./middlewares/errorHandler');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-// const allowedCors = [
-//   'http://legend.students.nomoredomains.sbs',
-//   'https://legend.students.nomoredomains.sbs',
-//   'http://api.legend.students.nomoredomains.sbs',
-//   'https://api.legend.students.nomoredomains.sbs',
-// ];
+const allowedCors = [
+  'http://legend.students.nomoredomains.sbs',
+  'https://legend.students.nomoredomains.sbs',
+  'http://api.legend.students.nomoredomains.sbs',
+  'https://api.legend.students.nomoredomains.sbs',
+  'http://api.legend.students.nomoredomains',
+  'https://api.legend.students.nomoredomains',
+  'http://api.legend.students.nomoredomains.sbs/',
+  'https://api.legend.students.nomoredomains.sbs/',
+];
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
 
-app.use(cors({
-  origin: [
-    'http://legend.students.nomoredomains.sbs',
-    'https://legend.students.nomoredomains.sbs',
-    'http://localhost:3001',
-    'http://localhost:3000',
-    'https://localhost:3001',
-    'http://localhost:3001',
-  ],
-  credentials: true,
-}));
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Credentials', true);
-//   const { origin } = req.headers;
-//   if (allowedCors.includes(origin)) {
-//     res.header('Access-Control-Allow-Origin', origin);
-//   }
-//   const { method } = req;
-//   const requestHeaders = req.headers['access-control-request-headers'];
+// app.use(cors({
+//   origin: [
+//     'http://legend.students.nomoredomains.sbs',
+//     'https://legend.students.nomoredomains.sbs',
+//     'http://localhost:3001',
+//     'http://localhost:3000',
+//     'https://localhost:3001',
+//     'http://localhost:3001',
+//   ],
+//   credentials: true,
+// }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', true);
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
-//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-//   if (method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-//     res.header('Access-Control-Allow-Headers', requestHeaders);
-//     res.end();
-//     return;
-//   }
-//   next();
-// });
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.end();
+    return;
+  }
+  next();
+});
 
 app.use(router);
 app.use(errorLogger);
