ba8495ee327cab07e4a88d560ac5e688cd57e005
Igor Manukhov <m_igor97@mail.ru>
2022-09-28 22:57:00 +0300

    cors old

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 const { errorHandler } = require('./middlewares/errorHandler');
 const router = require('./routes');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { cors } = require('./middlewares/cors');
+// const { cors } = require('./middlewares/cors');
 
 async function main() {
   try {
@@ -28,7 +29,17 @@ async function main() {
 
 main();
 
-app.use(cors);
+app.use(cors({
+  origin: [
+    'http://legend.students.nomoredomains.sbs',
+    'https://legend.students.nomoredomains.sbs',
+    'http://api.legend.students.nomoredomains.sbs',
+    'https://api.legend.students.nomoredomains.sbs',
+    'http://localhost:3000',
+    'http://localhost:3001',
+  ],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(requestLogger);
